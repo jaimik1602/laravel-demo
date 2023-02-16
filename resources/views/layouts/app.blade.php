7fe615ca65ef0eaa123f1483c6e79dcb84abcd7e
Jaimik <jaimik1603@gmail.com>
2023-02-16 20:00:52 +0530

    assets bundling

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,16 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" 
-    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title> @yield('title')</title>
+    {{-- <link rel="stylesheet" href="{{ asset('css/style.css') }}"> --}}
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
+
 <body>
     @include('layouts.header')
     @yield('content')
     @include('layouts.footer')
+    {{-- <script src="{{ asset('js/script.js') }}"></script> --}}
 </body>
+
 </html>
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,27 +1,27 @@
 @extends('layouts.app')
 
 @section('title')
-Users
+    Users
 @endsection
 
 @section('content')
-<h5 class="text-success">this is extended Users file</h5>
+    <h5 class="text-success">this is extended Users file</h5>
+   
 
-@php 
-echo 'this is core php';
-$number = 1;
-@endphp 
+    @php
+        echo 'this is core php';
+        $number = 1;
+    @endphp
 
-{{-- if else --}}
-@if ($number == 0)
-    yes
-@else
-    no
-@endif
+    {{-- if else --}}
+    @if ($number == 0)
+        yes
+    @else
+        no
+    @endif
 
-{{-- loops --}}
-@for ($i = 0; $i < 10; $i++)
-    {{ $i }}
-@endfor
-
+    {{-- loops --}}
+    @for ($i = 0; $i < 10; $i++)
+        {{ $i }}
+    @endfor
 @endsection
